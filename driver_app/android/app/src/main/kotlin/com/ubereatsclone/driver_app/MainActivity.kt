fe9398c477b34c69be2011012a44bdb34cbe97e9
Ketul Makwana <ketulm8@gmail.com>
2026-03-08 18:50:42 +0530

    Add riverpod generator and routing dependencies, closes #2

---
+++ driver_app/android/app/src/main/kotlin/com/ubereatsclone/driver_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ubereatsclone.driver_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ restaurant_dashboard/android/app/src/main/kotlin/com/ubereatsclone/restaurant_dashboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ubereatsclone.restaurant_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
